156701355fad97f2ae524c7633c7540af28ff4f8
bharani8143 <bharanidharan0309@gmail.com>
2022-01-08 03:18:31 +0530

    assessment

---
+++ jan eight.py
@@ -0,0 +1,80 @@
+# # find the given number is divisible by 3,5 (or) 3&5
+# a=3
+# b=5
+# c=int(input("enter the number "))
+# if   c%3==0 or c%5==0:
+#    print("true")
+# else:
+#     print("false")   
+
+    
+# enter the number 5
+# true
+# PS C:\Users\admin\Documents> & C:/Users/admin/AppData/Local/Microsoft/WindowsApps/python3.7.exe "c:/Users/admin/Documents/Python Scripts/jan eight.py"
+# enter the number 4
+# false
+# PS C:\Users\admin\Documents> & C:/Users/admin/AppData/Local/Microsoft/WindowsApps/python3.7.exe "c:/Users/admin/Documents/Python Scripts/jan eight.py"
+# enter the number 15
+# true
+# enter the number 3
+# true
+# PS C:\Users\admin\Documents>    
+   
+
+
+                 
+
+# nums = [5,10]
+# for num in nums:
+#     if num % 5 ==0:
+#         print(num)
+#         break
+# else:
+#     print("not found") 
+# 
+# 2)check an even or odd
+# num=int(input("enter a number"))
+# if (num%2)==0:
+#     print("even")
+# else:
+#     print("odd")  
+
+#  output      
+# enter a number5
+# odd
+# PS C:\Users\admin\Documents> & C:/Users/admin/AppData/Local/Microsoft/WindowsApps/python3.7.exe "c:/Users/admin/Documents/Python Scripts/jan eight.py"
+# enter a number2
+# even
+
+
+#compare two strings is equal or not equal
+# a=("hello")
+# b=("intellecto global services")
+# if a==b:
+#     print("false")
+# else:
+#     print("true")    
+
+# PS C:\Users\admin\Documents> & C:/Users/admin/AppData/Local/Microsoft/WindowsApps/python3.7.exe "c:/Users/admin/Documents/Python Scripts/jan eight.py"
+# true
+
+
+
+
+
+#greatest of three number
+x=float(input("enter three number:"))
+y=float(input("enter three number:"))
+z=float(input("enter three number:"))
+if x>y and x>z:
+    print("x is highest")
+elif y>z and y>x:
+    print("y is highest")
+else:
+    print("z is greater")
+
+
+
+
+
+
