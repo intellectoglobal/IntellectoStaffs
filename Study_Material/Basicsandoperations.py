1016a13775385acdd5fc0935c33b93a1c587b241
intellectoglobal <93407054+intellectoglobal@users.noreply.github.com>
2022-01-11 22:49:02 +0530

    updates

---
+++ Study_Material/Basicsandoperations.py
@@ -0,0 +1,173 @@
+# Welcome to Python
+#Printing Text
+print('Hello World')
+#output : Hello World
+print("Hello world")
+#output : Hello World
+
+#printing Numbers
+print(42)
+#output : 42
+
+#Simple Operation
+print(2+2)
+print(5+4-3)
+#output : 4
+#output : 6
+
+#Multiplication
+print(12*64)
+#output : 768
+
+# Division
+print(100/2) # using a single slash to divide numbers always produced decimal (float)
+#output : 50.0
+
+print(2*(3+4))
+#output : 14
+
+#Negative Numbers
+print(-8*6)
+#output : -48
+
+print(123456789*12345)
+#output : 1524074060205
+
+
+#DATA TYPES
+
+#STRINGS
+# use a single or double quotes to create string
+
+#INTEGERS
+#WHOLE NUMBERS CALLED INTEGER
+
+print(8+4-2)
+print(10*6)
+#output : 10
+#output : 60
+
+#FLOATS
+# THE RESULT OF DIVISION IS ALWAYS A FLOAT
+
+print(6/2)
+print(8/3)
+#output : 3.0
+#output : 2.6666666666666665
+
+#WHEN YOU ADD AN INTEGER TO A FLOAT THE RESULT IS ALWAYS FLOAT
+
+print(4.13*7)
+print(1.3+9.87)
+print(4+3.7)
+#output : 28.91
+#output : 11.17
+#output : 7.7
+
+#DATA TYPES
+
+#INTEGERS ARE WHOLE NUMBERS SUCH AS 8 AND 42
+#FLOATS ARE NUMBERS WITH A DECIMAL POINT LIKE 3.14
+#STRINGS ARE TEXT ENCLOSED IN SINGLE OR DOUBLE QUOTES LIKE ' ' AND " "
+
+#EXPONENTIATION
+#ITS DEFINED AS TWO ASTERISKS ** ARE USED TO RISE A NUMBER TO THE POWER OF ANOTHER
+
+print(2**5)
+#output : 32
+
+#CHAIN EXPONENTIATION
+#YOU CAN CHAIN EXPONENTIATIONS TOGETHER.
+
+print(2**3**2)
+#output : 512
+
+#WE CAN ALSO USE FLOATS IN EXPONENTIATION
+
+print(9**(1/2))
+#output = 3.0
+
+# FLOOR DIVISION
+# FIND THE QUOTION IN THE DIVISION
+# FLOOR DIVISION OPERATOR  //
+
+print(20//6)
+#output : 3
+
+print(4.2//1.4)
+#output : 3.0
+
+#REMAINDER
+#FIND REMAINDER IN THE DIVISION
+#WE USE THE MODULO OPERATOR %
+
+print(20%6)
+#output : 2
+
+print(1.25%0.5)
+#output : 0.25
+
+#STRINGS
+#STRINGS ARE TEXT OR INTEGER ENCLOSED IN SINGLE OR DOUBLE QUOTES LIKE ' ' AND " "
+print("Hi")
+#output : Hi
+print('Hi')
+#output : Hi
+print("42")
+#output : 42
+
+#BACKSLASH
+#IN PYTHON THE BACKSLASH \ IS A SPECIAL CHARACTER CALLED THE ESCAPE CHARACTER
+# IT IS USED TO REPRESENT CERTAINN THINGS IN A STRING SUCH AS NEW LINES , TABS AND OTHER USEFULL THINGS
+
+print('Brian\'s mother : He\'s not an angel.He\'s a very naughty boy!')
+#output : Brian's mother : He's not an angel.He's a very naughty boy!
+
+print("He said:\"cool!\".")
+#output : He said:"cool!".
+
+#NEW LINES
+#TO CREATE A NEW LINE WE USE \n
+print('One \nTwo \nThree')
+#output : 
+# One 
+# Two
+# Three
+
+#TAB
+# TO CREATE A TAB WE USE \t
+print("\t hey \t there")
+#output : hey   there
+
+#CARRIAGE RETURN
+# \r is the carriage return symbol
+print("Hi \r there")
+#output : there
+
+#ESCAPING
+#IF WE NEED SPECIAL CHARACTER \n IN THE OUTPUT ,WE CAN ESCAPE IT USING ANOTHER BACKSLASH \\n
+print("new line is \\n")
+#output : new line is \n
+
+print('\\r\\n')
+#output:\r\n
+
+#Hint \r\n for windows user & \n for Linux or unix
+
+
+#NEW LINES 
+#NEWLINES ARE AUTOMATICALLY ADDED FOR STRINGS CREATED USING THREE QUOTES """
+print("""this 
+is a 
+multiline 
+text""")
+#output :
+#this
+#is a
+#multiline
+#text
+
+
+
+
+  
