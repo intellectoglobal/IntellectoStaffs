6df65e428e1e3e81499e05522e7a7fb7ad4d4f6a
bharani8143 <bharanidharan0309@gmail.com>
2022-01-25 20:06:27 +0530

    tasks

---
+++ function_with_argument.py
@@ -0,0 +1,16 @@
+def greatest_three_number(x,y,z):
+   
+ if x>y and x>z:
+    print("x is highest")
+ elif y>z and y>x:
+   print("y is highest")
+ else:
+   print("z is greater")
+    
+
+
+greatest_three_number(1,2,3)
+
+OUTPUT:
+
+z is greater
